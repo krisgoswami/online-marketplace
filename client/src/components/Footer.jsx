05dafab50bb8696e89dd86cd115378295322b23b
Krishanu <krishgoswami15@gmail.com>
2023-11-03 17:33:24 +0530

    pages and components added

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='flex flex-col lg:flex-row bg-teal-500 lg:h-60'>
+                <div className='lg:w-1/2 ml-5 lg:ml-12 mt-5 lg:mt-10'>
+                    <p className='text-white text-2xl font-bold mb-3'>Book-A-Ground.com</p>
+                    <p className='text-white mb-5 w-96 lg:w-1/2 text-justify'>Book-A-Ground.com is an app where you can book grounds to play football and cricket with your friends and loved ones. Book a ground near you today! </p>
+                </div>
+                <div className='lg:w-1/4 mx-5 lg:ml-48 mt-5 lg:mt-10'>
+                    <p className='text-white text-lg font-bold mb-3'>Contact</p>
+                    <p className='text-white'>Email: <span className='text-teal-100 hover:text-teal-700 cursor-pointer'>JohnDoe@xyz.com</span></p>
+                </div>
+                <div className='lg:w-1/4 mx-5 lg:mr-10 mt-5 lg:mt-10'>
+                    <p className='text-white text-lg font-bold mb-3'>Links</p>
+                    <p className='text-teal-100 hover:text-teal-700 cursor-pointer mb-2'>FAQs</p>
+                    <p className='text-teal-100 hover:text-teal-700 cursor-pointer mb-2'>Facebook</p>
+                    <p className='text-teal-100 hover:text-teal-700 cursor-pointer mb-2'>Instagram</p>
+                    <p className='text-teal-100 hover:text-teal-700 cursor-pointer mb-2'>LinkedIn</p>
+                </div>
+            </div>
+
+            <div className='bg-teal-700 py-4 flex flex-col lg:flex-row items-center justify-between border-t border-gray-400'>
+                <p className='text-white font-semibold lg:ml-10'> &#169; Krishanu Goswami</p>
+                <div className='flex gap-2 lg:mr-10 lg:gap-10'>
+                    <a href='#' className='text-white font-medium'>Privacy Policy</a>
+                    <a href='#' className='text-white font-medium'>Terms</a>
+                    <a href='#' className='text-white font-medium'>Legal</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
